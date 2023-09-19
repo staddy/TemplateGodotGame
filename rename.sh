426820d631c05a3fc1dd26f00ff34be22844975b
Stanislav Ponkrashov <stas.ponkrashov@gmail.com>
2023-09-20 01:39:11 +0300

    Fix renaming script

--- rename.sh
+++ rename.sh
@@ -4,6 +4,7 @@ old_name="TemplateGodotGame"
 input_string="${1// /}"
 
 sed -i "s/${old_name}/${input_string}/g" ./${old_name}.sln
+sed -i s/assembly_name=\"${old_name}/assembly_name=\"${input_string}/g ./project.godot
 sed -i "s/${old_name}/$1/g" ./project.godot
 
 mv ./${old_name}.sln ./${input_string}.sln
