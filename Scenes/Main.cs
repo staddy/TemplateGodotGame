4728ea1e89105eab952a552c36fde0bd28ee20ec
Stanislav Ponkrashov <stas.ponkrashov@gmail.com>
2023-09-20 23:30:50 +0300

    Add screenshot making on F12 pressed while debugging

--- Scenes/Main.cs
+++ Scenes/Main.cs
@@ -12,5 +12,12 @@ public class Main : Node2D
     public override void _Input(InputEvent @event)
     {
         base._Input(@event);
+        if (@event.IsActionPressed("screenshot") && !OS.HasFeature("standalone"))
+        {
+            var screenshot = GetViewport().GetTexture().GetData();
+            screenshot.FlipY();
+            screenshot.Resize(screenshot.GetWidth() * 2, screenshot.GetHeight() * 2, Image.Interpolation.Nearest);
+            screenshot.SavePng($"res://Output/screenshots/{(long)(Time.GetUnixTimeFromSystem() * 1000)}.png");
+        }
     }
 }
